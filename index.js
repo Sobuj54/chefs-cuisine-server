4245721443f848045f1ac9cdfec9a0b24d3db09e
soubj <sobujahmed124@gmail.com>
2023-05-21 21:28:30 +0600

    created data json file of chef and showed data in server

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const chef = require("./data/chef.json");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("server is running properly.");
+});
+
+app.get("/chef", (req, res) => {
+  res.send(chef);
 });
 
 app.listen(port, () => {
